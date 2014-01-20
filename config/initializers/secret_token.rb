a94024e6025e6b925027a66e91105aad10af222d
skellz <sherwin.kelly@gmail.com>
2014-01-20 12:02:16 -0500

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OhlohNew::Application.config.secret_key_base = '8858b2195096a3ea0bc7d86b5085b43a66abaa1d95663c2e3f21a2a8d58b9cfefad4859216409b385f72e24c61b1b96aaf7499eb6e1acb121fee67613057748d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OhlohNew::Application.config.session_store :cookie_store, key: '_ohloh_new_session'
