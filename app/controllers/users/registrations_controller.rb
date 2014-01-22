21ee3f1c707882ff35e62d279b14bd3871bdd621
Parinda Darden <parinda.darden@gmail.com>
2014-01-21 20:24:44 -0500

    Fixed email_md5 issue and creating incoming badges

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,13 +1,15 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   def create
     super
-    email = email_md5(user.email)
+    email = @user.email_md5
     response = HTTParty.get("http://www.ohloh.net/accounts/#{email}.xml?api_key=Tc7aQRtw96YtYK3swieiQ")
-    badges_info = response.parsed_response
-    badges_info['badge'].each do |badge|
-      @user.badge_name << badge['name']
-      @user.badge_url << badge['image_url']
-      @user.badge_pipsurl << badge['pips_url']
+    badges_info = response.parsed_response['response']['result']['account']['badges']
+    badges_info['badge'].each do |uniq_badge|
+      Badges.find_or_create_by(name: uniq_badge['name']) do |badge|
+      badge.name = uniq_badge['name']
+      badge.url =  uniq_badge['image_url']
+      badge.pips_url = uniq_badge['pips_url']
+      end
     end
   end
 end
